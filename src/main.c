a45d152451fbfc39a9dde66c272b327db0a45d51
Philippe Thierry <phil@reseau-libre.net>
2018-12-17 15:33:27 +0100

    [bugfix] add support for cleaner size and @ transmission [bugfix] support for cleaner EOF in DFU read mode

--- src/main.c
+++ src/main.c
@@ -9,6 +9,9 @@
 #include "api/print.h"
 #include "wookey_ipc.h"
 
+uint16_t blocknum = 0;
+uint32_t total_read = 0;
+
 /*
  * We use the local -fno-stack-protector flag for main because
  * the stack protection has not been initialized yet.
@@ -19,6 +22,7 @@
  */
 #define FLASH_DEBUG 0
 #define FLASH_BUF_SIZE 4096
+#define FLASH_SIZE 800000 // 800K flash size, test size
 uint8_t flash_buf[FLASH_BUF_SIZE] = { 0 };
 
 int _main(uint32_t task_id)
@@ -152,13 +156,13 @@ int _main(uint32_t task_id)
      * Main waiting loopt. The task main thread is awoken by any external
      * event such as ISR or IPC.
      */
-      struct dataplane_command dataplane_command_wr;
-      struct dataplane_command dataplane_command_ack = { MAGIC_DATA_WR_DMA_ACK, 0, 0 };
+      struct sync_command_data dataplane_command_wr;
+      struct sync_command_data dataplane_command_ack = { 0 };
       t_ipc_command ipc_mainloop_cmd = { 0 };
 
       while (1) {
           uint8_t id = id_dfucrypto;
-          logsize_t size = sizeof(t_ipc_command);
+          logsize_t size = sizeof(struct sync_command_data);
 
           ret = sys_ipc(IPC_RECV_SYNC, &id, &size, (char*)&ipc_mainloop_cmd);
 
@@ -171,41 +175,60 @@ int _main(uint32_t task_id)
 
               case MAGIC_DATA_WR_DMA_REQ:
                   {
-                    dataplane_command_wr = ipc_mainloop_cmd.dataplane_cmd;
+                      dataplane_command_wr = ipc_mainloop_cmd.sync_cmd_data;
 #if FLASH_DEBUG
-                    printf("!!!!!!!!!!! received DMA write command to FLASH: @:%x size: <todo>\n",
-                            dataplane_command_wr.num_sectors);
+                      printf("!!!!!!!!!!! received DMA write command to FLASH: blocknum:%x size: %x\n",
+                              dataplane_command_wr.data.u16[1], dataplane_command_wr.data.u16[0]);
 #endif
-                    dataplane_command_ack.magic = MAGIC_DATA_WR_DMA_ACK;
+                      /*returning the number of bytes read */
+                      dataplane_command_ack.magic = MAGIC_DATA_WR_DMA_ACK;
+                      dataplane_command_ack.data.u16[0] = dataplane_command_wr.data.u16[0];
+                      dataplane_command_ack.data_size = 1;
 
-                    ret = sys_ipc(IPC_SEND_SYNC, id_dfucrypto, sizeof(struct dataplane_command), (const char*)&dataplane_command_ack);
+                      ret = sys_ipc(IPC_SEND_SYNC, id_dfucrypto, sizeof(struct sync_command_data), (const char*)&dataplane_command_ack);
 
-                    if (ret != SYS_E_DONE) {
-                        printf("Error ! unable to send back DMA_WR_ACK to crypto!\n");
-                    }
-                    break;
+                      if (ret != SYS_E_DONE) {
+                          printf("Error ! unable to send back DMA_WR_ACK to crypto!\n");
+                      }
+                      break;
 
                   }
 
               case MAGIC_DATA_RD_DMA_REQ:
                   {
-                    dataplane_command_wr = ipc_mainloop_cmd.dataplane_cmd;
+                      uint16_t read_data = 0;
+                      uint32_t prev_total = total_read;
+                      dataplane_command_wr = ipc_mainloop_cmd.sync_cmd_data;
 #if FLASH_DEBUG
-                    printf("received DMA read command to FLASH: @[sector] :%x @[bytes]: %x size: %d\n",
-                            dataplane_command_wr.sector_address,
-                            dataplane_command_wr.sector_address * 512,
-                            dataplane_command_wr.num_sectors);
+
+                      printf("!!!!!!!!!!! received DMA read command to FLASH: blocknum:%x size: %x\n",
+                              blocknum, dataplane_command_wr.data.u16[0]);
 #endif
-                    // read request.... let's read then...
+                      total_read += dataplane_command_wr.data.u16[0];
+                      if (total_read > FLASH_SIZE) {
+                          /* reinit for next upload if needed */
+                          total_read = 0;
+                          blocknum = 0;
+                          read_data = FLASH_SIZE - prev_total;
+#if FLASH_DEBUG
+                          printf("end of flash read, residual size is %x\n", read_data);
+#endif
+                      } else {
+                          read_data = dataplane_command_wr.data.u16[0];
+                          blocknum  += 1;
+                      }
+                      // read request.... let's read then...
 
-                    dataplane_command_ack.magic = MAGIC_DATA_RD_DMA_ACK;
+                      /*returning the number of bytes read */
+                      dataplane_command_ack.magic = MAGIC_DATA_RD_DMA_ACK;
+                      dataplane_command_ack.data.u16[0] = read_data;
+                      dataplane_command_ack.data_size = 1;
 
-                    ret = sys_ipc(IPC_SEND_SYNC, id_dfucrypto, sizeof(struct dataplane_command), (const char*)&dataplane_command_ack);
+                      ret = sys_ipc(IPC_SEND_SYNC, id_dfucrypto, sizeof(struct sync_command_data), (const char*)&dataplane_command_ack);
 
-                    if (ret != SYS_E_DONE) {
-                        printf("Error ! unable to send back DMA_WR_ACK to crypto!\n");
-                    }
-                    break;
+                      if (ret != SYS_E_DONE) {
+                          printf("Error ! unable to send back DMA_WR_ACK to crypto!\n");
+                      }
                     break;
 
                   }
